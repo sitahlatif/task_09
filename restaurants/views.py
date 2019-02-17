60166ceb479626d00026ca3755f3eee373621a27
sitahlatif <sitah27@hotmail.com>
2019-02-17 03:20:52 +0300

    Task09

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm, SignUpForm , SignInForm
+from django.contrib.auth import authenticate , login, logout
 
 def signup(request):
-    
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user_obj = form.save(commit =False)
+            user_obj.set_password(user_obj.password)
+            user_obj.save()
+            login(request, user_obj)
+            return redirect('restaurant-list')
+    context = {
+        "form":form,
+    }
     return render(request, 'signup.html', context)
 
 def signin(request):
-    
-    return 
+    form = SignInForm()
+    if request.method == "POST":
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            my_username= form.cleaned_data['username']
+            my_password = form.cleaned_data['password']
+            user_obj= authenticate(username= my_username, password=my_password)
+            if user_obj is not None :
+                login(request,user_obj)
+                return redirect('restaurant-list')
+    context = {
+        "form":form,
+    }
+    return render(request, 'signin.html', context)
+ 
 
 def signout(request):
-    
-    return 
+    logout(request)
+    return redirect('restaurant-list') 
 
 def restaurant_list(request):
     context = {
